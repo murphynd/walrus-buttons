0ecf897bda497ec07fa0b53aeafec4dbea019d1c
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-11 13:16:40 -0700

    added everything like a goober

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#initially-showing").toggle();
-    $("#initially-hidden").toggle();
+  $(".toggle-walrus").click(function() {
+    $("#walrus-initially-showing").toggle();
+    $("#walrus-initially-hidden").toggle();
+  });
+  $(".toggle-panda").click(function() {
+    $("#panda-initially-showing").toggle();
+    $("#panda-initially-hidden").toggle();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
